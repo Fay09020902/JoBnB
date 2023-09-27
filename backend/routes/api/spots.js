1cf875c6f50603f0c606af8d7a83b697a33fd584
yinjiao <fyj121322@gmail.com>
2023-09-27 12:26:38 -0700

    Add API routes for Spots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -4,13 +4,102 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const { User, Spot } = require('../../db/models');
 const e = require('express');
 
 const router = express.Router();
 
+const validateSignup = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .withMessage('Street address is required'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage('City address is required'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('State address is required'),
+    check('lat')
+        .optional()
+        .isFloat({
+            min: -90,
+            max: 90,
+          })
+        .withMessage('Latitude is not valid'),
+    check('lng')
+        .optional()
+        .isFloat({
+            min: -180,
+            max: 180,
+          })
+        .withMessage('Longitude is not valid'),
+    check('name')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 1, max: 49 })
+        .withMessage('Name must be less than 50 characters'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Description is required'),
+    check('price')
+        .exists({ checkFalsy: true })
+        .isFloat({
+            min: 0,
+          })
+        .withMessage('Price per day is required'),
+];
 
+//Get all Spots
+router.get(
+    "/",
+    validateSignup,
+    async (req, res, next) => {
+        const spots = await Spot.findAll()
+        return res.json({
+            Spots: spots
+          });
+    })
+//Create a Spot
+router.post(
+    "/",
+    validateSignup,
+    requireAuth,
+    async (req, res, next) => {
+        const {user} = req;
+        let { address, city, state, country, lat, lng, name, description, price } =
+        req.body;
+        const currentUser = await User.findByPk(user.id);
+        const newSpot = await currentUser.createSpot({
+            "ownerId":user.id, address, city, state, country, lat, lng, name, description, price});
+        return res.status(201).json(newSpot);
+    }
+);
 
+//Get all Spots owned by the Current User
+router.get(
+    "/sessions",
+    validateSignup,
+    requireAuth,
+    async (req, res, next) => {
+        ///console.log("req is ", req)
+        const {user} = req
+        console.log("userid is", user.id)
+        const spots = await Spot.findAll(
+            {where: {ownerId: user.id}}
+        )
+        return res.json({
+            Spots: spots
+          });
+    });
+
+//Get details of a Spot from an id
+router.get(
+    "/:spotId",
+    validateSignup,
+    async (req, res, next) => {
+        const {spotId} = req.params
+        const spot = await Spot.findByPk(spotId)
+        return res.json(spot);
+    });
 // router.delete("/:id", async (req, res, next) => {
 //     const treeToDelete = await User.findByPk(req.params.id);
 
