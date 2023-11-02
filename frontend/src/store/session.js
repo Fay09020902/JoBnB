122c5f32b3b336393405499a3b800b205e9e9bca
yinjiao <fyj121322@gmail.com>
2023-11-02 16:34:00 -0700

    add login thunk action

---
+++ frontend/src/store/session.js
@@ -0,0 +1,44 @@
+import { csrfFetch } from "./csrf";
+
+const SET_USER = "session/setUser";
+const REMOVE_USER = "session/removeUser";
+
+const setUser = (user) => {
+  return {
+    type: SET_USER,
+    payload: user,
+  };
+};
+
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const response = await csrfFetch("/api/session", {
+      method: "POST",
+      body: JSON.stringify({
+        credential,
+        password,
+      }),
+    });
+    if(response.ok) {
+        const data = await response.json();
+        dispatch(setUser(data.user));
+        return response;
+    }
+    return response;
+  };
+
+const initialState = {user: null}
+
+const sessionReducer = (state = initialState, action) => {
+    let newState
+    switch (action.type) {
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;
