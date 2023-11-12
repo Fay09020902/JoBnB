af9f8b8a331571d1085437b21f19f77dcf449e95
yinjiao <fyj121322@gmail.com>
2023-11-11 18:30:41 -0800

    update delete review

--- frontend/src/component/ConfirmReviewDeleteModal/index.js
+++ frontend/src/component/ConfirmReviewDeleteModal/index.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import * as reviewActions from "../../store/reviews";
 import { useDispatch} from "react-redux";
-import { useParams} from 'react-router-dom';
 import { useModal } from "../../context/Modal";
 import {deleteReviewThunk} from '../../store/reviews'
 
@@ -9,12 +8,17 @@ import {deleteReviewThunk} from '../../store/reviews'
 function ConfirmReviewDeleteModal({spotid, reviewid}) {
   const dispatch = useDispatch();
   const { closeModal } = useModal();
+  const [errors, setErrors] = useState({});
 
 
   const handleDelete = (e) => {
     if(e.target.value === 'Yes'){
-      return dispatch(deleteReviewThunk(reviewid,spotid))
+      return dispatch(deleteReviewThunk(reviewid, spotid))
       .then(closeModal)
+      .catch(async err => {
+        const data = await err.json();
+        setErrors({message:data.message});
+     });
     } else {
       dispatch(closeModal)
     }
@@ -26,6 +30,7 @@ function ConfirmReviewDeleteModal({spotid, reviewid}) {
     <div className ='form-container'>
       <h1>Confirm Delete</h1>
       <p>Are you sure you want to delete this review?</p>
+      {errors && <p className="error-message">{errors.message}</p>}
       <button value="Yes" onClick={handleDelete}>Yes</button>
        <button value="No" onClick={handleDelete}>No</button>
     </div>
